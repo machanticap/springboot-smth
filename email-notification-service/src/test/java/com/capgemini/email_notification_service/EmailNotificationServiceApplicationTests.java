c86db73ecc7d9c75f2fb9fc3dbe1e4a8f7dcac16
Achanti <maxwell.achanti@capgemini.com>
2026-08-25 14:43:46 -0400

    Adding spring boot initializing project folder

---
+++ email-notification-service/src/test/java/com/capgemini/email_notification_service/EmailNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.email_notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailNotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
