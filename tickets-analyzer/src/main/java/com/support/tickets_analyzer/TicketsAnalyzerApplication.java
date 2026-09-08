98719131ee5fef5c6bfa2058d0ae7d1e211bcc1e
Cian Leonard <cian.leonard@capgemini.com>
2026-09-08 10:59:22 -0400

    Initialized Springboot Project

---
+++ tickets-analyzer/src/main/java/com/support/tickets_analyzer/TicketsAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.support.tickets_analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketsAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketsAnalyzerApplication.class, args);
+	}
+
+}
---
+++ tickets-analyzer/src/test/java/com/support/tickets_analyzer/TicketsAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.support.tickets_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketsAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
